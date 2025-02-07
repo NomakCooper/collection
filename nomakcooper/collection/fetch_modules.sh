3fa8a0aa41d0e8f7b85cdf2824af5a38c83a350d
Nocchia <133043574+NomakCooper@users.noreply.github.com>
2025-02-07 22:23:52 +0000

    rename sar module and change date format

--- nomakcooper/collection/fetch_modules.sh
+++ nomakcooper/collection/fetch_modules.sh
@@ -4,7 +4,7 @@ set -e
 echo "Fetching latest Python modules from GitHub..."
 
 # Define GitHub raw URLs for Python modules
-SAR_FACTS_URL="https://raw.githubusercontent.com/NomakCooper/sar_info/main/library/sar_facts.py"
+SAR_FACTS_URL="https://raw.githubusercontent.com/NomakCooper/sar_facts/main/library/sar_facts.py"
 EXA_FACTS_URL="https://raw.githubusercontent.com/NomakCooper/exa_facts/main/library/exa_facts.py"
 
 # Define the local destination
